3e36f982d5c5f7c13dff493de2dc45d0ebaa0d17
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2023-05-19 19:22:35 +0200

    Sending cart data to firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-129a6-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,21 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
